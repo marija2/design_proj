5969df5cfe7863c3392c2d9ef0755bbae8b93c6a
Marija Trifkovic <mt3786@nyu.edu>
2021-11-20 21:39:37 -0500

    c

--- server/index.js
+++ server/index.js
@@ -3,12 +3,10 @@ var path = require('path')
 // var connectionString = "postgres://newuser:password@127.0.0.1:5432/postgres";
 // var connectionString = process.env.DATABASE_URL
 
-var pgClient = new pg.Client(process.env.DATABASE_URL)//{
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+var pgClient = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 // var pgClient = new pg.Client(connectionString);
 pgClient.connect();
